79fec59af1b15bbdde1e8db7f170cd21baa44348
Phildo <phildo211@gmail.com>
2019-03-22 15:54:44 -0500

    fix oversat

--- src/board.js
+++ src/board.js
@@ -1735,7 +1735,7 @@ var board = function()
       for(var i = 0; i < 50; i++)
       {
         var t = self.tiles[self.tiles_i(self.bounds_tx+randIntBelow(self.bounds_tw),self.bounds_ty+randIntBelow(self.bounds_th))];
-        t.nutrition += rand();
+        t.nutrition = rand();
         t.nutrition *= t.nutrition;
         t.nutrition *= t.nutrition;
         t.nutrition *= t.nutrition;
