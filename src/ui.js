e1f89a7087b39d79e14baaafbbe26bff3c23140d
Phildo <phildo211@gmail.com>
2019-07-16 14:12:17 -0500

    nutrition bar fix

--- src/ui.js
+++ src/ui.js
@@ -1029,14 +1029,16 @@ var inspector = function()
       else
         gg.ctx.fillText("Soil Nutrition:",x,u.nutrition_y);
 
-                                   draw_nutrition_bar(          x,u.nutrition_bar_y,self.w-self.pad*2,min(t.nutrition,water_fouled_threshhold), nutrition_color);
       if(t.type == TILE_TYPE_LAKE)
       {
+        draw_nutrition_bar(          x,u.nutrition_bar_y,self.w-self.pad*2,min(t.nutrition,water_fouled_threshhold), nutrition_color);
         draw_remaining_nutrition_bar(x,u.nutrition_bar_y,self.w-self.pad*2,min(t.nutrition,water_fouled_threshhold),water_fouled_threshhold,vlight_gray);
         //this is too clever for its own good
         draw_remaining_nutrition_bar(x,u.nutrition_bar_y,self.w-self.pad*2,min(t.nutrition,water_fouled_threshhold),t.nutrition,green);
         draw_remaining_nutrition_bar(x,u.nutrition_bar_y,self.w-self.pad*2,max(t.nutrition,water_fouled_threshhold),water_fouled_threshhold+food_nutrition,light_green);
       }
+      else
+       draw_nutrition_bar(x,u.nutrition_bar_y,self.w-self.pad*2,t.nutrition, nutrition_color);
 
       gg.ctx.fillStyle = gg.font_color;
       if(t.fertilizer)
