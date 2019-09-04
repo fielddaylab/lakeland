b3d8670968caf9d0532218a19aef794c04b5893e
John <johnmccloskey2010@hotmail.com>
2019-09-04 12:26:51 -0500

    Logging v9 - Update version to v9.

--- src/logging.js
+++ src/logging.js
@@ -1,6 +1,6 @@
 window.Logger = function(init){
   self = this;
-  self.mySlog = new slog("LAKELAND",8);
+  self.mySlog = new slog("LAKELAND",9);
   //var pako = require('pako');
   //Constants
   self.NUTRITION_DIFFERENCE = 2;
